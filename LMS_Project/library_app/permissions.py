8645c52a61d0abbf6ca2811576367250ad83062e
SAGAR PATIL <sagarpatil8025@gmail.com>
2025-04-15 22:22:29 +0530

    Revert "all files"
    This reverts commit ea25f58c50ccffd12168f3d73bef9a304067592b.

--- LMS_Project/library_app/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdminUser(BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
